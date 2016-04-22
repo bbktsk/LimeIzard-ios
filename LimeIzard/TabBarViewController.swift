ccf5e09a2ba6d775b89542169f4200ecaf4dae8e
vytick <vytick@gmail.com>
2016-04-22 20:53:16 +0200

    adding statusBar

--- LimeIzard/TabBarViewController.swift
+++ LimeIzard/TabBarViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class TabBarViewController: UITabBarController {
+    
+    weak var statusBar: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("adasad")
-        // Do any additional setup after loading the view.
+        let statusBar = UIView.loadFromNibNamed("StatusBar")!
+        view.addSubview(statusBar)
+        statusBar.snp_makeConstraints() { make in
+            make.height.equalTo(44)
+            make.bottom.equalTo(view.snp_bottom).offset(-49)
+            make.left.right.equalTo(view)
+        }
+        self.statusBar = statusBar
     }
 
    
