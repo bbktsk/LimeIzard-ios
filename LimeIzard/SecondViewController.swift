8cb2a5dcc65c083b70bd5f91e7a9ff5993ada9d2
Pavel Hamrik <inscient@Aviateur.local>
2016-04-22 17:41:57 +0200

    2nd init

---
+++ LimeIzard/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LimeIzard
+//
+//  Created by Pavel Hamrik on 22/04/16.
+//  Copyright © 2016 LimeIzards. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
