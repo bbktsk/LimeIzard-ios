5dbf56fb381c1b87695e94e1e18fd8bd255723ec
Pavel Hamrik <inscient@Aviateur.local>
2016-04-23 00:52:34 +0200

    StatusBar UI finished.

--- LimeIzard/TabBarViewController.swift
+++ LimeIzard/TabBarViewController.swift
@@ -16,6 +16,19 @@ class TabBarViewController: UITabBarController {
     weak var taglineButton: UIButton!
     var available = false
     
+
+    let feelings = [
+        "happy": "\u{1F600} Happy",
+        "cheerful": "\u{1F60A} Cheerful",
+        "thinking": "\u{1F914} Thinking",
+        "custom": "\u{1F4AC} Custom"
+    ]
+
+    let availabilities = [
+        "available": "\u{1F917} Available",
+        "sleeping": "\u{1F634} Sleeping"
+    ]
+
     override func loadView() {
         super.loadView()
         
@@ -30,26 +43,29 @@ class TabBarViewController: UITabBarController {
         
         
         let availableButton = UIButton()
-        availableButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        availableButton.setTitleColor(UIColor(hex: 0x43de0a), forState: .Normal)
         statusBar.addSubview(availableButton)
         availableButton.snp_makeConstraints() {make in
-            make.left.equalTo(statusBar.snp_left).offset(5)
-            make.width.equalTo(150)
-            make.height.equalTo(30)
+            make.left.equalTo(statusBar.snp_left).offset(15)
+            make.width.equalTo(120)
+            make.height.equalTo(42)
             make.centerY.equalTo(statusBar.snp_centerY)
         }
+        availableButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
         self.availableButton = availableButton
-        availableButton.imageEdgeInsets = UIEdgeInsets(top: 5.0, left: 5.0, bottom: 5.0, right: 5.0)
+        //availableButton.imageEdgeInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
         
         let taglineButton = UIButton()
         taglineButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
         statusBar.addSubview(taglineButton)
         taglineButton.snp_makeConstraints() {make in
             make.left.equalTo(availableButton.snp_right).offset(10)
-            make.width.equalTo(150)
-            make.height.equalTo(30)
+            make.right.equalTo(statusBar.snp_right)
+            make.height.equalTo(42)
             make.centerY.equalTo(statusBar.snp_centerY)
         }
+        taglineButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+        taglineButton.titleLabel?.lineBreakMode = .ByTruncatingTail
         self.taglineButton = taglineButton
         
         let separator = UIView()
@@ -66,14 +82,14 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setAvailability(false)
+        setAvailability(true)
         
-        let image = UIImage(named: "first") as UIImage!
-        availableButton.setImage(image, forState: .Normal)
+        //let image = UIImage(named: "first") as UIImage!
+        //availableButton.setImage(image, forState: .Normal)
         availableButton.addTarget(self, action: #selector(TabBarViewController.changeAvailability), forControlEvents: .TouchUpInside)
         
         taglineButton.addTarget(self, action: #selector(TabBarViewController.changeTagline), forControlEvents: .TouchUpInside)
-        taglineButton.setTitle("happy", forState: .Normal)
+        taglineButton.setTitle(feelings["happy"], forState: .Normal)
     }
 
    
@@ -85,13 +101,11 @@ class TabBarViewController: UITabBarController {
     func setAvailability(available: Bool) {
         self.available = available
         if available {
-            let image = UIImage(named: "first") as UIImage!
-            availableButton.setImage(image, forState: .Normal)
-            availableButton.setTitle("available", forState: .Normal)
+            availableButton.setTitle(availabilities["available"], forState: .Normal)
+            availableButton.setTitleColor(UIColor(hex: 0x43de0a), forState: .Normal)
         } else {
-            let image = UIImage(named: "second") as UIImage!
-            availableButton.setImage(image, forState: .Normal)
-            availableButton.setTitle("nope", forState: .Normal)
+            availableButton.setTitle(availabilities["sleeping"], forState: .Normal)
+            availableButton.setTitleColor(UIColor(hex: 0xdd0b38), forState: .Normal)
         }
     }
     
@@ -104,15 +118,25 @@ class TabBarViewController: UITabBarController {
     func changeTagline() {
         
         
-        let optionMenu = UIAlertController(title: nil, message: "Choose Option", preferredStyle: .ActionSheet)
+        let optionMenu = UIAlertController(title: nil, message: "How Are You Feeling?", preferredStyle: .ActionSheet)
         
-        let deleteAction = UIAlertAction(title: "Delete", style: .Default, handler: {
+        let happyAction = UIAlertAction(title: feelings["happy"], style: .Default, handler: {
             (alert: UIAlertAction!) -> Void in
-            print("File Deleted")
+            self.taglineButton.setTitle(self.feelings["happy"], forState: .Normal)
         })
-        let saveAction = UIAlertAction(title: "Save", style: .Default, handler: {
+        let cheerfulAction = UIAlertAction(title: feelings["cheerful"], style: .Default, handler: {
             (alert: UIAlertAction!) -> Void in
-            print("File Saved")
+            self.taglineButton.setTitle(self.feelings["cheerful"], forState: .Normal)
+        })
+
+        let thinkingAction = UIAlertAction(title: feelings["thinking"], style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.taglineButton.setTitle(self.feelings["thinking"], forState: .Normal)
+        })
+
+        let customAction = UIAlertAction(title: feelings["custom"], style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            self.composeTagline()
         })
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
@@ -120,13 +144,50 @@ class TabBarViewController: UITabBarController {
             print("Cancelled")
         })
         
-        optionMenu.addAction(deleteAction)
-        optionMenu.addAction(saveAction)
+        optionMenu.addAction(happyAction)
+        optionMenu.addAction(cheerfulAction)
+        optionMenu.addAction(thinkingAction)
+        optionMenu.addAction(customAction)
         optionMenu.addAction(cancelAction)
         
-        //self.presentViewController(optionMenu, animated: true, completion: nil)
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+
+
+    }
+
+    func composeTagline() {
+
+
+        let composeAlert = UIAlertController(title: "Customize", message: "You're feeling...", preferredStyle: .Alert)
         
+        let confirmAction = UIAlertAction(title: "Confirm", style: .Default, handler: {
+            (alert: UIAlertAction!) -> Void in
+            let loginTextField = composeAlert.textFields![0] as UITextField
+            if (loginTextField.text ?? "").isEmpty {
+                self.taglineButton.setTitle("\u{1F4AC} Unsure. Probably.", forState: .Normal)
+            }
+            else {
+                self.taglineButton.setTitle("\u{1F4AC} " + loginTextField.text!, forState: .Normal)
+            }
+
+        })
+
+        composeAlert.addTextFieldWithConfigurationHandler { (textField) in
+            textField.placeholder = "Chatty, maybe?"
+            textField.autocapitalizationType = .Sentences
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: {
+            (alert: UIAlertAction!) -> Void in
+        })
+
+        composeAlert.addAction(confirmAction)
+        composeAlert.addAction(cancelAction)
+        composeAlert.preferredAction = confirmAction
+
+        self.presentViewController(composeAlert, animated: true, completion: nil)
         
     }
+
 
 }
