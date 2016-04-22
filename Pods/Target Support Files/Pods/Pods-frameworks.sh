decf5812785c5bf646225ef4114992a21884a260
vytick <vytick@gmail.com>
2016-04-23 00:52:26 +0200

    changing fb framework version

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,10 +85,6 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
-  install_framework "Pods/Bolts.framework"
-  install_framework "Pods/FBSDKCoreKit.framework"
-  install_framework "Pods/FBSDKLoginKit.framework"
-  install_framework "Pods/FBSDKShareKit.framework"
   install_framework "${PODS_ROOT}/KontaktSDK/Cocoapods/iOS/KontaktSDK.framework"
   install_framework "Pods/SVProgressHUD.framework"
   install_framework "Pods/SnapKit.framework"
@@ -96,10 +92,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
-  install_framework "Pods/Bolts.framework"
-  install_framework "Pods/FBSDKCoreKit.framework"
-  install_framework "Pods/FBSDKLoginKit.framework"
-  install_framework "Pods/FBSDKShareKit.framework"
   install_framework "${PODS_ROOT}/KontaktSDK/Cocoapods/iOS/KontaktSDK.framework"
   install_framework "Pods/SVProgressHUD.framework"
   install_framework "Pods/SnapKit.framework"
