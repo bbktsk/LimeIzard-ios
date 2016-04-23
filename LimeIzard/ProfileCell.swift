19d2b6dfba285290de2df0eab26cb94f78b514c9
Pavel Hamrik <inscient@Aviateur.local>
2016-04-23 08:29:46 +0200

    We are all doomed.

---
+++ LimeIzard/ProfileCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.swift
+//  LimeIzard
+//
+//  Created by Pavel Hamrik on 23/04/16.
+//  Copyright © 2016 LimeIzards. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- LimeIzard/ProfileViewController.swift
+++ LimeIzard/ProfileViewController.swift
@@ -9,17 +9,62 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    var profile = [
+        [
+            "Name": "Martin",
+            "Gender": "Male"
+        ],
+        [
+            "Label": "Value"
+        ]
+    ]
+    
+    let profileSections = [
+        "You",
+        "Other"
+    ]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let profileCellIdentifier = "ProfileCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
 }
 
+
+extension ProfileViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return profile.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if let cell = tableView.dequeueReusableCellWithIdentifier(profileCellIdentifier, forIndexPath: indexPath) as? ProfileCell {
+            //cell.preferenceLabel?.text = profile[indexPath.section][indexPath.row]
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return profile.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return profileSections[section]
+    }
+    
+    
+    
+}
